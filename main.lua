97923b09709353e65584d5c2688564f314aa67c8
Andy Durdin <me@andy.durdin.net>
2017-01-20 21:05:13 +0000

    Hot reload when main.lua changes.

--- main.lua
+++ main.lua
@@ -9,9 +9,18 @@ end
 
 function love.update(dt)
     -- Every frame:
+    hotReload()
+
     if love.keyboard.isDown("space") then
         -- Print to console
         print("Your are pressing space")
+    end
+end
+
+function love.keypressed(key, unicode)
+    -- Quit on escape
+    if key == "escape" then
+        love.event.quit()
     end
 end
 
@@ -22,4 +31,29 @@ function love.draw()
     love.graphics.draw(educational_image, 0, 0)
     love.graphics.setColor(0,0,0,255)
     love.graphics.print("FRACK THE PLANET", 300, 10)
+end
+
+
+-- Hot reload
+
+hotReloadFrameCounter = 0
+mainLastModified = love.filesystem.getLastModified("main.lua")
+
+function hotReload()
+    hotReloadFrameCounter = (hotReloadFrameCounter + 1) % 10
+    if hotReloadFrameCounter == 0 then
+        if mainLastModified ~= love.filesystem.getLastModified("main.lua") then
+            mainLastModified = love.filesystem.getLastModified("main.lua")
+            ok, mainCode = pcall(love.filesystem.load,"main.lua")  -- Load program
+            if not ok then
+                print("Load error: "..err)
+            else
+                print("Reloaded")
+                ok, err = pcall(mainCode) -- Execute program
+                if not ok then
+                    print("Execute error: "..err)
+                end
+            end
+        end
+    end
 end
