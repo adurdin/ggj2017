4584ad68d86b8fe7930ae4b48fb6a989c5af0450
Andy Durdin <me@andy.durdin.net>
2017-01-21 11:09:29 +0000

    Terrain all falls down

--- main.lua
+++ main.lua
@@ -59,7 +59,7 @@ function love.update(dt)
     player.vel = player.vel * (1 - 0.01 * dt * 1000)
 
     -- update terrain
-    terrain:update()
+    terrain:update(dt)
 end
 
 function love.keypressed(key, unicode)
@@ -85,7 +85,7 @@ function love.draw()
     sonarShader:send("radius", sonarVars.radius)
     sonarShader:send("maxTime", sonarVars.maxTime)
     sonarShader:send("currentTime", sonarVars.currentTime)
-    sonarShader:send("densityMap", terrain.readImage)
+    sonarShader:send("densityMap", terrain.image)
     love.graphics.setShader(sonarShader)
     -- Every frame:
     -- show an educational image
@@ -151,7 +151,13 @@ TERRAIN_GAS_ALPHA = 128
 -- void is where gas used to be, but we pumped it out.
 TERRAIN_VOID_ALPHA = 129
 -- sky is the endless emptiness above all the dirt.
-TERRAIN_SKY_ALPHA = 129
+TERRAIN_SKY_ALPHA = 130
+-- convert dirt alpha to velocity (pixels/second) with this value and multiplification
+TERRAIN_ALPHA_TO_VELOCITY = 60
+-- super falling force (pixels/second^2)
+TERRAIN_GRAVITY = 16
+-- maximum super falling speed (pixels/second)
+TERRAIN_TERMINAL_VELOCITY = 10
 
 terrain = {}
 
@@ -190,14 +196,11 @@ end
 function createTerrain(terrain)
     terrain.width = TERRAIN_WIDTH
     terrain.height = TERRAIN_HEIGHT
-    terrain.readData = love.image.newImageData(terrain.width, terrain.height)
-    terrain.writeData = love.image.newImageData(terrain.width, terrain.height)
+    terrain.data = love.image.newImageData(terrain.width, terrain.height)
 
     -- create a terrain and copy it into the second data buffer
-    terrain.readData:mapPixel(generateTerrainPixel)
-    terrain.writeData:paste(terrain.readData, 0, 0, 0, 0, terrain.width, terrain.height)
-    terrain.readImage = love.graphics.newImage(terrain.readData)
-    terrain.writeImage = love.graphics.newImage(terrain.writeData)
+    terrain.data:mapPixel(generateTerrainPixel)
+    terrain.image = love.graphics.newImage(terrain.data)
 
     -- surface is the y coordinate of the topmost piece of dirt in the terrain
     -- FIXME: this should sample the initial data, but for the moment it's just flat
@@ -211,20 +214,112 @@ function createTerrain(terrain)
     -- we'll later only wake them with a shockwave
     terrain.awakeColumns={}
     for x=0,(terrain.width-1) do
-        terrain.awakeColumns[x] = true
+        terrain.awakeColumns[x] = false
     end
+    -- start with one column awake
+    terrain.awakeColumns[math.floor(terrain.width / 2)] = true
+
     return terrain
 end
 
-function terrain:update()
-    -- FIXME(andy): simulate the terrain here!
+function terrain:update(dt)
+    -- collapse a single column of terrain
+    function collapseColumn(x, dt)
+        -- start at the bottom of the column.
+        -- move pixels down according to their velocity.
+        -- update their velocity (if not 0) by dt.
+        -- they collide if they would exceed maxY;
+        -- if they collide, their velocity is capped to maxVelocity.
+        -- after each pixel is moved, maxVelocity becomes its velocity,
+        -- and maxY becomes its y - 1
+        local maxVelocity = 0 -- at the bottom of the terrain, all motion must stop
+        local maxY = self.height - 1 -- and pixels can't fall past the bottom of the terrain
+
+        local readY = self.height - 1
+        local writeY = self.height - 1
+
+        local stayAwake = false
+
+        while readY >= 0 or writeY >= 0 do
+            local r, g, b, a
+            if readY >= 0 then
+                r, g, b, a = terrain.data:getPixel(x, readY)
+
+                if a >= TERRAIN_DIRT_ALPHA_MIN and a <= TERRAIN_DIRT_ALPHA_MAX then
+                    local velocity = a * TERRAIN_ALPHA_TO_VELOCITY
+                    if velocity == 0 then
+                        -- this pixel isn't going to fall
+                        -- but pixels above will stop if they hit this one
+                        maxVelocity = 0
+                        writeY = writeY - 1
+                    else
+                        -- something is falling
+                        stayAwake = true
+
+                        local newY = math.floor(readY + velocity * dt)
+                        local newVelocity = math.floor(velocity + TERRAIN_GRAVITY * dt)
+
+                        -- check for collisions and limit distance and velocity
+                        if newY >= writeY then
+                            newY = writeY
+                            newVelocity = math.min(newVelocity, maxVelocity)
+                        end
+                        -- pixels above can't fall faster than this one if they hit it
+                        maxVelocity = newVelocity
+
+                        -- fill with void up to where it's fallen to
+                        for y=writeY,newY+1,-1 do
+                            terrain.data:setPixel(x, y, 0, 0, 0, TERRAIN_VOID_ALPHA)
+                        end
+
+                        -- and move the pixel
+                        local newA = math.floor(newVelocity / TERRAIN_ALPHA_TO_VELOCITY)
+                        terrain.data:setPixel(x, newY, r, g, b, newA)
+
+                        writeY = newY - 1
+                    end
+                elseif a == TERRAIN_SKY_ALPHA then
+                    -- Only sky from here up. Save the surface level and fall back to skyfilling
+                    self.surface[x] = readY
+                    readY = -1
+                else
+                    -- FIXME: later we want to keep track of the size of the void below each pixel maybe,
+                    -- so we can look it up without scanning the data again
+                    -- but for now do nothing
+                end
+                readY = readY - 1
+            else
+                -- it's sky all the way up
+                terrain.data:setPixel(x, writeY, 0, 140, 254, TERRAIN_SKY_ALPHA)
+                writeY = writeY - 1
+            end
+        end
+
+        return stayAwake
+    end
+
+    -- collapse each awake column
+    local newAwakeColumns = {}
+    for x=0,(self.width-1) do
+        newAwakeColumns[x] = false
+    end
+    for x=0,(self.width-1) do
+        if self.awakeColumns[x] then
+            -- collapse this column
+            newAwakeColumns[x] = collapseColumn(x, dt)
+            -- wake up the columns beside it
+            if x > 0 then
+                newAwakeColumns[x - 1] = true
+            end
+            if x < self.width then
+                newAwakeColumns[x + 1] = true
+            end
+        end
+    end
+    self.awakeColumns = newAwakeColumns
 
     -- refresh the terrain image from its data
-    terrain.writeImage:refresh()
-
-    -- swap the read and write buffers
-    local t = terrain.readData; terrain.readData = terrain.writeData; terrain.writeData = t;
-    local t = terrain.readImage; terrain.readImage = terrain.writeImage; terrain.writeImage = t;
+    terrain.image:refresh()
 end
 
 function terrain:draw(x, y, toCanvas)
@@ -237,7 +332,7 @@ function terrain:draw(x, y, toCanvas)
     love.graphics.setBlendMode("replace", "premultiplied")
     love.graphics.setColor(255,255,255,255)
     love.graphics.setColorMask(true, true, true, toCanvas)
-    love.graphics.draw(terrain.readImage, x, y)
+    love.graphics.draw(terrain.image, x, y)
 
     -- restore state
     love.graphics.setBlendMode(unpack(prevBlendMode))
@@ -257,4 +352,19 @@ function player:draw()
     love.graphics.setColor(255, 140, 0, 255)
     love.graphics.rectangle("fill", self.x % TERRAIN_WIDTH - TERRAIN_WIDTH, y, 50, 50, 0)
     love.graphics.rectangle("fill", self.x % TERRAIN_WIDTH, y, 50, 50, 0)
+end
+
+-- Debug
+
+function dump(o)
+    if type(o) == 'table' then
+        local s = '{ '
+        for k,v in pairs(o) do
+            if type(k) ~= 'number' then k = '"'..k..'"' end
+            s = s .. '['..k..'] = ' .. dump(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
 end
